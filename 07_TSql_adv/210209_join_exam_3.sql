5dea54732fe5787ae5ed720ae7a83e114a21fcc3
yfla980107 <yfla980107@gmail.com>
2021-02-09 15:54:02 +0900

    21/02/09 DB-Transact 고급

---
+++ 07_TSql_adv/210209_join_exam_3.sql
@@ -0,0 +1,18 @@
+﻿
+/*select * from stdTbl;
+select * from clubTbl;
+select * from clubRegTbl;
+*/
+-- Stander SQL (표준)
+select s.id, s.stdName, c.clubName, c.buildingNum
+  from stdTbl as s
+  inner join clubRegTbl as r
+	on s.id = r.std_id
+  inner join clubTbl as c
+	on r.club_id = c.id;
+
+-- inner join 약식 (SQL Server T-SQL)
+select s.id, s.stdName, c.clubName, c.buildingNum
+  from stdTbl as s, clubRegTbl as r, clubTbl as c
+ where s.id = r.std_id
+   and r.club_id = c.id;
