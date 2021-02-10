225cd0b50f90f7b7250bfc799f9429f4086a9b57
yfla980107 <yfla980107@gmail.com>
2021-02-10 10:50:40 +0900

    21/02/10 while

---
+++ 07_TSql_adv/210210_while_exam.sql
@@ -0,0 +1,17 @@
+﻿declare @i int, @hap bigint = 0
+set @i = 0;
+
+while (@i <= 100)
+begin
+	if (@i % 10 = 0) --10의 배수
+	begin
+		print concat ('10의 배수', @i)
+		set @i += 1
+		continue;
+	end
+	set @hap += @i
+	if (@hap > 1000) break;
+	set @i += 1;
+end
+
+print @hap;
