e1fd2efdd483e7f9918cc84f102c90206aaa7483
yfla980107 <yfla980107@gmail.com>
2021-02-10 11:36:01 +0900

    21/02/10 if ~ else

---
+++ 07_TSql_adv/210210_tsql_program1.sql
@@ -0,0 +1,11 @@
+﻿declare @val1 int; --변수선언
+set @val1 = 100; --변수 값 대입
+
+if @val1 = 100 -- 값 비교
+begin
+	print '@val1 100임'; -- 텍스트 메세지로 그냥 보여줌
+end
+else
+begin
+	select '@val1 100아님'; -- 결과를 가상의 테이블형태로 보여줌
+end
