91ac3e129eff0c2be9b78085e6708959b3e11214
yfla980107 <yfla980107@gmail.com>
2021-02-10 17:21:45 +0900

    21/02/10 schema

---
+++ 08_table_view/210210_schema_practice.sql
@@ -0,0 +1,16 @@
+﻿use schemaDB;
+go
+
+create table users.userTBL(id int not null); -- 스키마는 편리성과 쉽게 구분하기 위해 씀
+go
+create table sales.buyTBL(id int not null);
+go
+create table sales.prodTBL(pid int);
+go
+
+create table normalTBL(nid int not null);
+go
+
+select * from users.userTBL;
+select * from sales.buyTBL;
+select * from dbo.normalTBL;
