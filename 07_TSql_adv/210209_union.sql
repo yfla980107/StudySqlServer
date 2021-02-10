864a76bbb025b65bd38434694e505d39857a5d7c
yfla980107 <yfla980107@gmail.com>
2021-02-10 10:11:12 +0900

    21/02/09 union

---
+++ 07_TSql_adv/210209_union.sql
@@ -0,0 +1,9 @@
+﻿-- Union (중복값 생략)
+select clubName as bName, buildingNum as bLocation from clubTbl
+union
+select stdName, region from stdTbl;
+
+--union all (중복값 같이 보여줌)
+select stdName, region from stdTbl
+intersect --union all
+select stdName, region from stdTbl;
