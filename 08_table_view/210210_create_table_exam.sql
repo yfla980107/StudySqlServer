979db8acd2d49bf8767866b499940c89da970987
yfla980107 <yfla980107@gmail.com>
2021-02-10 15:28:05 +0900

    21/02/10 create

--- 08_table_view/210210_create_table_exam.sql
+++ 08_table_view/210210_create_table_exam.sql
@@ -13,19 +13,29 @@ create database tableDB;
 */
 use tableDB;
 go
-create table userTBL
+drop table buyTBL, userTBL;
+go
+create table userTBL -- 부모
 (
-	userID		char(8),		-- 아이디
-	userName	nvarchar(10),	-- 이름
-	birthYear	int,			-- 출생연도
-	addr		nchar(2),		-- 지역
-	mobile1		char(3),		-- 휴대폰 국번
-	mobile2		char(8),		-- 휴대폰 번호
-	height		smallint,		-- 키
-	mDate		date			-- 회원가입일
+	userID		char(8)			not null		primary key,		-- 아이디
+	userName	nvarchar(10)	not null,		-- 이름
+	birthYear	int				not null default year(getdate())  ,		-- 출생연도
+	addr		nchar(2)		not null default '서울',		-- 지역
+	mobile1		char(3)			null,			-- 휴대폰 국번
+	mobile2		char(8)			null,			-- 휴대폰 번호
+	height		smallint		null  default 170 ,			-- 키
+	email		varchar(150)	not null unique, -- 이메일추가 210210 14:25 유니크는 유일한값만 입력가능
+	mDate		date			null,			-- 회원가입일
 );
 go
-create table buyTBL
+create table buyTBL -- 자식
 (
+	num			int				not null	identity(1, 1) primary key,		-- 순번(PK)
+	userID		char(8)			not null
+		foreign key references userTBL(userID),		-- 아이디
+	prodName	nchar(6)		not null,		-- 품명
+	groupName	nchar(4)		null,			-- 분류
+	price		int				not null,		-- 단가
+	amount		smallint		not null,		-- 수량
 );
 go
