19a729fbb4e3e17e97651446f2a1dda138b70a9b
yfla980107 <yfla980107@gmail.com>
2021-02-15 13:48:10 +0900

    21/02/15 transaction query

---
+++ 10_Transaction/210215_Transaction_query_sample.sql
@@ -0,0 +1,48 @@
+﻿USE [sqlDB]
+GO
+
+BEGIN TRY --오류나면 CATCH 구문으로 이동(ROLLBACK 수행) 
+	BEGIN TRAN;
+
+	INSERT INTO [dbo].[userTbl]
+			   ([userID]
+			   ,[userName]
+			   ,[birthYear]
+			   ,[addr]
+			   ,[mobile1]
+			   ,[mobile2]
+			   ,[height]
+			   ,[mDate])
+		 VALUES
+			   ('GJY'
+			   ,'권지용'
+			   ,1988
+			   ,'서울'
+			   ,'010'
+			   ,'98989898'
+			   ,175
+			   ,getdate());
+
+	INSERT INTO [dbo].[buyTbl]
+			   ([userID]
+			   ,[prodName]
+			   ,[groupName]
+			   ,[price]
+			   ,[amount])
+		 VALUES
+			   ('GJY'
+			   ,'선글라스'
+			   ,NULL
+			   ,8000000
+			   ,1);
+
+	COMMIT;
+END TRY
+
+BEGIN CATCH
+	ROLLBACK;
+END CATCH
+
+GO
+
+
