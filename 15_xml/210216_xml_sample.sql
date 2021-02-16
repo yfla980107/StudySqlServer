85367a214fbaeb93aa27a816abc692370f441ad5
yfla980107 <yfla980107@gmail.com>
2021-02-16 15:43:46 +0900

    21/02/16 xml

---
+++ 15_xml/210216_xml_sample.sql
@@ -0,0 +1,7 @@
+﻿select u.userID, u.userName, u.birthYear, u.addr, u.height, b.prodName, b.price
+  from usertbl as u
+inner join buyTbl as b
+	on u.userID = b.userID
+ where height > 170
+ for json auto; -- json으로 변환 또는 
+ for xml auto; -- xml으로 변환
